bc2e5b6236a6617099cb7ff0e1fe4dd60ada6960
kamil.palka <cabeq1236@gmail.com>
2024-01-25 20:05:56 +0100

    add 2nd level layer and weight, biases dash animation

--- visualizer.js
+++ visualizer.js
@@ -6,10 +6,31 @@ class Visualizer {
     const width = ctx.canvas.width - margin * 2;
     const height = ctx.canvas.height - margin * 2;
 
-    Visualizer.drawLevel(ctx, network.levels[0], left, top, width, height);
+    const levelHeight = height / network.levels.length;
+
+    for (let i = network.levels.length - 1; i >= 0; i--) {
+      const levelTop =
+        top +
+        lerp(
+          height - levelHeight,
+          0,
+          network.levels.length == 1 ? 0.5 : i / (network.levels.length - 1)
+        );
+
+      ctx.setLineDash([7, 3]);
+      Visualizer.drawLevel(
+        ctx,
+        network.levels[i],
+        left,
+        levelTop,
+        width,
+        levelHeight,
+        i == network.levels.length - 1 ? ["🠉", "🠈", "🠊", "🠋"] : []
+      );
+    }
   }
 
-  static drawLevel(ctx, level, left, top, width, height) {
+  static drawLevel(ctx, level, left, top, width, height, outputLabels) {
     const right = left + width;
     const bottom = top + height;
 
@@ -38,7 +59,7 @@ class Visualizer {
       ctx.fill();
       ctx.beginPath();
       ctx.arc(x, bottom, nodeRadius * 0.6, 0, Math.PI * 2);
-      ctx.fillStyle = "white";
+      ctx.fillStyle = getRGBA(inputs[i]);
       ctx.fill();
     }
 
@@ -51,7 +72,7 @@ class Visualizer {
       ctx.fill();
       ctx.beginPath();
       ctx.arc(x, top, nodeRadius * 0.6, 0, Math.PI * 2);
-      ctx.fillStyle = "white";
+      ctx.fillStyle = getRGBA(outputs[i]);
       ctx.fill();
 
       ctx.beginPath();
@@ -61,6 +82,18 @@ class Visualizer {
       ctx.setLineDash([3, 3]);
       ctx.stroke();
       ctx.setLineDash([]);
+
+      if (outputLabels[i]) {
+        ctx.beginPath();
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "white";
+        ctx.font = nodeRadius * 1.5 + "px Arial";
+        ctx.fillText(outputLabels[i], x, top + nodeRadius * 0.1);
+        ctx.lineWidth = 0.5;
+        ctx.strokeText(outputLabels[i], x, top + nodeRadius * 0.1);
+      }
     }
   }
 
